0ef353ee9fbd7b5dc7121f9aa17941c52805b4db
gaoshuaixing <gaoshuaixing@bilibili.com>
2020-09-21 11:58:02 +0800

    1

--- src/views/account/center/page/AllWeb.vue
+++ src/views/account/center/page/AllWeb.vue
@@ -1,56 +1,51 @@
 <template>
-    <a-card
-      style="margin-top: 24px"
-      :bordered="false"
-      title="标准列表">
+  <a-card
+    style="margin-top: 24px"
+    :bordered="false"
+    title="标准列表">
 
-      <div slot="extra">
-        <a-radio-group v-model="status">
-          <a-radio-button value="all">全部</a-radio-button>
-          <a-radio-button value="processing">进行中</a-radio-button>
-          <a-radio-button value="waiting">等待中</a-radio-button>
-        </a-radio-group>
-        <a-input-search style="margin-left: 16px; width: 272px;" />
-      </div>
-
-      <div class="operate">
-        <a-button type="dashed" style="width: 100%" icon="plus" @click="add">添加</a-button>
-      </div>
-
-      <a-list size="large" :pagination="{showSizeChanger: true, showQuickJumper: true, pageSize: 5, total: 50}">
-        <a-list-item :key="index" v-for="(item, index) in data">
-          <a-list-item-meta :description="item.description">
-            <a-avatar slot="avatar" size="large" shape="square" :src="item.avatar"/>
-            <a slot="title">{{ item.title }}</a>
-          </a-list-item-meta>
-          <div slot="actions">
-            <a @click="edit(item)">编辑</a>
+    <div slot="extra">
+      <!-- <a-radio-group v-model="status">
+        <a-radio-button value="all">全部</a-radio-button>
+        <a-radio-button value="processing">进行中</a-radio-button>
+        <a-radio-button value="waiting">等待中</a-radio-button>
+      </a-radio-group>
+      <a-input-search style="margin-left: 16px; width: 272px;" /> -->
+    </div>
+    <a-list size="large" :pagination="{showSizeChanger: true, showQuickJumper: true, pageSize: 5, total: 50}">
+      <a-list-item :key="index" v-for="(item, index) in data">
+        <a-list-item-meta :description="item.description">
+          <a-avatar slot="avatar" size="large" shape="square" :src="item.avatar"/>
+          <a slot="title">{{ item.title }}</a>
+        </a-list-item-meta>
+        <div slot="actions">
+          <a @click="edit(item)">编辑</a>
+        </div>
+        <div slot="actions">
+          <a-dropdown>
+            <a-menu slot="overlay">
+              <a-menu-item><a>编辑</a></a-menu-item>
+              <a-menu-item><a>删除</a></a-menu-item>
+            </a-menu>
+            <a>更多<a-icon type="down"/></a>
+          </a-dropdown>
+        </div>
+        <div class="list-content">
+          <div class="list-content-item">
+            <span>Owner</span>
+            <p>{{ item.owner }}</p>
           </div>
-          <div slot="actions">
-            <a-dropdown>
-              <a-menu slot="overlay">
-                <a-menu-item><a>编辑</a></a-menu-item>
-                <a-menu-item><a>删除</a></a-menu-item>
-              </a-menu>
-              <a>更多<a-icon type="down"/></a>
-            </a-dropdown>
+          <div class="list-content-item">
+            <span>开始时间</span>
+            <p>{{ item.startAt }}</p>
           </div>
-          <div class="list-content">
-            <div class="list-content-item">
-              <span>Owner</span>
-              <p>{{ item.owner }}</p>
-            </div>
-            <div class="list-content-item">
-              <span>开始时间</span>
-              <p>{{ item.startAt }}</p>
-            </div>
-            <div class="list-content-item">
-              <a-progress :percent="item.progress.value" :status="!item.progress.status ? null : item.progress.status" style="width: 180px" />
-            </div>
+          <div class="list-content-item">
+            <a-progress :percent="item.progress.value" :status="!item.progress.status ? null : item.progress.status" style="width: 180px" />
           </div>
-        </a-list-item>
-      </a-list>
-    </a-card>
+        </div>
+      </a-list-item>
+    </a-list>
+  </a-card>
 
 </template>
 
@@ -125,31 +120,6 @@ export default {
     }
   },
   methods: {
-    add () {
-      this.$dialog(TaskForm,
-        // component props
-        {
-          record: {},
-          on: {
-            ok () {
-              console.log('ok 回调')
-            },
-            cancel () {
-              console.log('cancel 回调')
-            },
-            close () {
-              console.log('modal close 回调')
-            }
-          }
-        },
-        // modal props
-        {
-          title: '新增',
-          width: 700,
-          centered: true,
-          maskClosable: false
-        })
-    },
     edit (record) {
       console.log('record', record)
       this.$dialog(TaskForm,
