8d9c4031cfc23e9e49b5b3b38caf58d3d1de8a8a
gaoshuaixing <gaoshuaixing@bilibili.com>
2020-09-23 19:25:51 +0800

    去除foot而

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -1,12 +1,6 @@
 <template>
   <global-footer class="footer custom-render">
     <template v-slot:links>
-      <a href="#" target="_blank"></a>
-      <a href="#" target="_blank"></a>
-      <a href="#" target="_blank"></a>
-    </template>
-    <template v-slot:copyright>
-      <a href="#" target="_blank">box</a>
     </template>
   </global-footer>
 </template>
