098871d56f09b9600188544121b4892296febcc9
gaoshuaixing <gaoshuaixing@bilibili.com>
2020-09-24 17:59:33 +0800

    style

--- src/views/account/center/page/AllWeb.vue
+++ src/views/account/center/page/AllWeb.vue
@@ -15,17 +15,22 @@
     <a-list :loading="loading" size="large" :pagination="paginationOpt">
       <a-list-item :key="index" v-for="(item, index) in webList">
         <a-list-item-meta :description="item.desc">
-          <a-avatar slot="avatar" size="large" shape="circle" :src="item.img"/>
+          <a-avatar slot="avatar" size="default" shape="circle" :src="item.img"/>
           <a slot="title">{{ item.name }}</a>
         </a-list-item-meta>
-        <div slot="actions">
-          <a @click="handleEdit(item)">添加</a>
-        </div>
-        <div class="list-content">
+        <div class="list-times">
           <div class="list-content-item">
             <a-icon type="star-o" style="margin-right: 8px" />
-            <span>100</span>
+            <span>{{ item.col_times }}</span>
           </div>
+        </div>
+        <div class="list-view">
+          <div class="list-content-item">
+            <a :href="item.url">查看</a>
+          </div>
+        </div>
+        <div class="list-view">
+          <a @click="handleEdit(item)">添加</a>
         </div>
       </a-list-item>
     </a-list>
@@ -42,16 +47,13 @@
 </template>
 
 <script>
-// 演示如何使用 this.$dialog 封装 modal 组件
 import WebForm from './modules/WebSaveForm'
-import Info from './components/Info'
 import { outApi } from '@/api/main'
 
 export default {
   name: 'StandardList',
   components: {
-    WebForm,
-    Info
+    WebForm
   },
   data () {
     return {
@@ -143,49 +145,6 @@ export default {
             }).catch(err => {
               console.log('err:', err)
             })
-          // new Promise((resolve, reject) => {
-          //     setTimeout(() => {
-          //       resolve()
-          //     }, 1000)
-          //   }).then(res => {
-          //     this.visible = false
-          //     this.confirmLoading = false
-
-          //     this.$message.info('新增成功')
-          //   })
-          // if (values.id > 0) {
-          //   // 修改 e.g.
-          //   new Promise((resolve, reject) => {
-          //     setTimeout(() => {
-          //       resolve()
-          //     }, 1000)
-          //   }).then(res => {
-          //     this.visible = false
-          //     this.confirmLoading = false
-          //     // 重置表单数据
-          //     form.resetFields()
-          //     // 刷新表格
-          //     this.$refs.table.refresh()
-
-          //     this.$message.info('修改成功')
-          //   })
-          // } else {
-          //   // 新增
-          //   new Promise((resolve, reject) => {
-          //     setTimeout(() => {
-          //       resolve()
-          //     }, 1000)
-          //   }).then(res => {
-          //     this.visible = false
-          //     this.confirmLoading = false
-          //     // 重置表单数据
-          //     form.resetFields()
-          //     // 刷新表格
-          //     this.$refs.table.refresh()
-
-          //     this.$message.info('新增成功')
-          //   })
-          // }
         } else {
           this.confirmLoading = false
         }
@@ -201,7 +160,14 @@ export default {
     height: 48px;
     line-height: 48px;
 }
-
+.list-times {
+  width: 10%;
+  text-align: center;
+}
+.list-view {
+  width: 10%;
+  text-align: center;
+}
 .list-content-item {
     color: rgba(0, 0, 0, .45);
     display: inline-block;
