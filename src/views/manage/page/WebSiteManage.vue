c3f2aa14e0348fb06ba84338bd62fe9ee5ef4492
gaoshuaixing <gaoshuaixing@bilibili.com>
2020-09-28 20:16:15 +0800

    1

--- src/views/manage/page/WebSiteManage.vue
+++ src/views/manage/page/WebSiteManage.vue
@@ -1,85 +1,212 @@
 <template>
-  <a-table :columns="columns" :data-source="data">
-    <a slot="name" slot-scope="text">{{ text }}</a>
-    <span slot="tags" slot-scope="tags">
-      <a-tag
-        v-for="tag in tags"
-        :key="tag"
-        :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
-      >
-        {{ tag.toUpperCase() }}
-      </a-tag>
-    </span>
-    <span slot="action" slot-scope="text, record">
-      <a>Invite 一 {{ record.name }}</a>
-      <a-divider type="vertical" />
-      <a>Delete</a>
-      <a-divider type="vertical" />
-      <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
-    </span>
-  </a-table>
+  <div>
+    <standard-form-row title="" block style="padding-bottom: 11px;">
+      <a-radio-group v-model="status" @change="handleChangeType(status)">
+        <a-radio-button value="uwsid">默认</a-radio-button>
+        <a-radio-button value="uwsid">最新</a-radio-button>
+      </a-radio-group>
+      <a-input-search style="margin-left: 16px; width: 272px;" @search="handleSearch"/>
+    </standard-form-row>
+    <a-card
+      style="margin-top: 0px"
+      :bordered="true"
+      title="">
+      <div class="operate">
+        <a-button type="dashed" style="width: 100%">添加</a-button>
+      </div>
+      <a-list :loading="loading" size="large" :pagination="paginationOpt">
+        <a-list-item :key="index" v-for="(item, index) in webList" style="padding-top: 10px;padding-bottom: 10px;">
+          <a-list-item-meta :description="item.desc">
+            <a-avatar
+              style="color: #f56a00;backgroundColor:#fff"
+              slot="avatar"
+              size="default"
+              shape="square"
+              :src="item.img"
+              icon="tag">
+            </a-avatar>
+            <a slot="title">{{ item.name }}</a>
+          </a-list-item-meta>
+          <div class="list-times">
+            <div class="list-content-item">
+              <a-icon type="star-o" style="margin-right: 8px" />
+              <span>{{ item.col_times }}</span>
+            </div>
+          </div>
+          <div class="list-view">
+            <div class="list-content-item">
+              <a :href="item.url" target="_blank">查看</a>
+            </div>
+          </div>
+          <div class="list-view">
+            <a @click="handleEdit(item)">添加</a>
+          </div>
+        </a-list-item>
+      </a-list>
+      <web-form
+        ref="webSaveModal"
+        :visible="visible"
+        :loading="confirmLoading"
+        :model="mdl"
+        @cancel="handleCancel"
+        @ok="handleOk"
+      />
+    </a-card>
+  </div>
 </template>
-<script>
-const columns = [
-  {
-    dataIndex: 'name',
-    key: 'name',
-    title: 'name',
-    scopedSlots: { customRender: 'name' }
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age'
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address'
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    scopedSlots: { customRender: 'tags' }
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    scopedSlots: { customRender: 'action' }
-  }
-]
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer']
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser']
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher']
-  }
-]
+<script>
+import WebForm from './modules/WebSaveForm'
+import { outApi } from '@/api/main'
+import { StandardFormRow } from '@/components'
 
 export default {
+  name: 'StandardList',
+  components: {
+    WebForm,
+    StandardFormRow
+  },
   data () {
     return {
-      data,
-      columns
+      webList: {},
+      webPageList: {},
+      paginationOpt: {
+        current: 1,
+        total: 0,
+        pageSize: 10,
+        'show-quick-jumper': true,
+        onChange: (current) => {
+          this.paginationOpt.current = current
+          this.queryParam.page = current
+          this.getManageUserSite()
+        }
+      },
+      loading: true,
+      // 查询参数
+      queryParam: {
+        page: 1,
+        sort: 'uwsid'
+      },
+      status: 'uwsid',
+      visible: false,
+      confirmLoading: false,
+      mdl: null
+    }
+  },
+  mounted () {
+    this.getManageUserSite()
+  },
+  methods: {
+    handleSearch (value) {
+      this.queryParam.sort = 'uwsid'
+      this.queryParam.page = 1
+      this.getManageUserSite()
+    },
+    handleChangeType (status) {
+      this.status = status
+      this.queryParam.sort = status
+      this.queryParam.page = 1
+      this.getManageUserSite()
+    },
+    handleEdit (record) {
+      this.visible = true
+      this.mdl = {
+        id: record.uwsid,
+        name: record.name,
+        url: record.url
+      }
+    },
+    getAllWebs () {
+      const params = {
+        out_url: 'manageUserSite',
+        method: 'POST',
+        data: {
+          page: this.queryParam.page
+        }
+      }
+      outApi(params).then(res => {
+        this.loading = false
+        if (res.code !== 0) {
+          return
+        }
+        this.webPageList = res.data && res.data.list
+        this.webList = this.webPageList.data
+        this.paginationOpt.total = this.webPageList.total
+        this.paginationOpt.pageSize = this.webPageList.per_page
+        }).catch(err => {
+          console.log('err:', err)
+        })
+    },
+    handleCancel () {
+      this.visible = false
+      this.confirmLoading = false
+      const form = this.$refs.webSaveModal.form
+    },
+    handleOk () {
+      // console.log('webInfo', webInfo)
+      const form = this.$refs.webSaveModal.form
+      this.confirmLoading = true
+      form.validateFields((errors, values) => {
+        console.log('getManageUserSite values:', values)
+        if (!errors) {
+          const params = {
+            out_url: 'saveSite',
+            method: 'POST',
+            data: {
+              uwsid: values.uwsid,
+              type: values.type,
+              name: values.name,
+              url: values.url,
+              sort: 0
+            }
+          }
+          outApi(params).then(res => {
+            this.confirmLoading = false
+            if (res.code !== 0) {
+              this.$message.info('添加失败')
+              return
+            }
+              this.visible = false
+              this.$message.info('添加成功')
+            }).catch(err => {
+              console.log('err:', err)
+            })
+        } else {
+          this.confirmLoading = false
+        }
+      })
     }
   }
 }
 </script>
+
+<style lang="less" scoped>
+.ant-avatar-lg {
+    width: 48px;
+    height: 48px;
+    line-height: 48px;
+}
+.list-times {
+  width: 10%;
+  text-align: left;
+}
+.list-view {
+  width: 10%;
+  text-align: center;
+}
+.list-content-item {
+    color: rgba(0, 0, 0, .45);
+    display: inline-block;
+    vertical-align: middle;
+    font-size: 14px;
+    margin-left: 40px;
+    span {
+        line-height: 20px;
+    }
+    p {
+        margin-top: 4px;
+        margin-bottom: 0;
+        line-height: 22px;
+    }
+}
+</style>
