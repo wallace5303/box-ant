c169488e684dd2a76b2180b2342fb2c403e053a9
gaoshuaixing <gaoshuaixing@bilibili.com>
2020-10-19 16:51:08 +0800

    web style

--- src/views/home/single/Web.vue
+++ src/views/home/single/Web.vue
@@ -36,7 +36,8 @@
                   <a-card :bordered="false" :body-style="{ padding: 0 }">
                     <a-card-meta >
                       <div slot="title" class="card-title">
-                        <a-avatar style="color: #f56a00;backgroundColor:#fff;" shape="square" size="small" :src="web.img" icon="tag"></a-avatar>
+                        <a-avatar v-if="web.img" shape="square" size="small" :src="web.img"></a-avatar>
+                        <a-avatar v-else :style="web.style || 'color: #fff;backgroundColor:#2BA245;'" shape="square" size="small" :src="web.img">{{web.name.substr(0,1)}}</a-avatar>
                         <a class="overflow" v-if="web.url" :href="web.url" target="_blank">
                           <a-tooltip>
                             <template v-if="web.name.length > 5" slot="title">
