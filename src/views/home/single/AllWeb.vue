c86fa89987abf32022aef168339e5ec5a8d817cd
gaoshuaixing <gaoshuaixing@bilibili.com>
2020-10-19 17:14:06 +0800

    1

--- src/views/home/single/AllWeb.vue
+++ src/views/home/single/AllWeb.vue
@@ -26,14 +26,7 @@
       <a-list :loading="loading" size="large" :pagination="paginationOpt">
         <a-list-item :key="index" v-for="(item, index) in webList" style="padding-top: 10px;padding-bottom: 10px;">
           <a-list-item-meta :description="item.desc">
-            <a-avatar
-              style="color: #f56a00;backgroundColor:#fff"
-              slot="avatar"
-              size="default"
-              shape="square"
-              :src="item.img"
-              icon="tag">
-            </a-avatar>
+            <a-avatar slot="avatar" size="default" shape="square" :src="item.img"></a-avatar>
             <a slot="title">{{ item.name }}</a>
           </a-list-item-meta>
           <div class="list-times">
