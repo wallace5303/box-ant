76a7d2aa155bfa4c03946dec454fe8d2d7823bfc
gaoshuaixing <gaoshuaixing@bilibili.com>
2020-11-04 16:40:35 +0800

    百度

--- src/views/home/single/Web.vue
+++ src/views/home/single/Web.vue
@@ -16,6 +16,15 @@
           自定义添加
         </a-button>
       </router-link>
+      <a-input-search
+        placeholder=""
+        style="margin-left: 16px; width: 272px;"
+        @search="baiduSearch">
+        <!-- <a-button slot="enterButton" style="backgroundColor:#4e6ef2; color:#fff;"> -->
+        <a-button slot="enterButton">
+          百度一下
+        </a-button>
+      </a-input-search>
     </standard-form-row>
     <a-card
       v-if="token"
@@ -100,6 +109,7 @@ export default {
     return {
       token: null,
       loading: true,
+      inputText: '',
       webList: {},
       visible: false,
       confirmLoading: false,
@@ -123,6 +133,56 @@ export default {
     handleAdd () {
       this.visible = true
     },
+    baiduSearch (value) {
+      this.inputText = value
+      window.open('https://www.baidu.com/s?wd=' + this.inputText, '_blank')
+      this.inputText = ''
+    },
+    // gotoItem (item) {
+    //   window.open('https://www.baidu.com/s?wd=' + item, '_blank')
+    //   this.inputText = ''
+    // },
+    // show (ev) {
+    //   if (ev.keyCode === 38 || ev.keyCode === 40) {
+    //     if (this.nowIndex < -1) {
+    //       return false
+    //     }
+    //     if (this.nowIndex !== this.result.length && this.nowIndex !== -1) {
+    //       this.inputText = this.result[this.nowIndex]
+    //     }
+    //     return false
+    //   }
+    //   if (ev.keyCode === 13) {
+    //     window.open('https://www.baidu.com/s?wd=' + this.inputText, '_blank')
+    //     this.inputText = ''
+    //   }
+    //   this.text = this.inputText
+    //   this.$http.post('https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su',
+    //     { wd: this.inputText },
+    //     {
+    //       emulateJSON: true
+    //   }).then(res => {
+    //     this.result = res.data.s
+    //   })
+    // },
+    // down () {
+    //   this.nowIndex++
+    //   if (this.nowIndex === this.result.length) {
+    //     this.nowIndex = -1
+    //     this.inputText = this.text
+    //   }
+    // },
+    // up () {
+    //   this.nowIndex--
+    //   if (this.nowIndex < -1) {
+    //     this.nowIndex = -1
+    //     return false
+    //   }
+    //   if (this.nowIndex === -1) {
+    //     this.nowIndex = this.result.length
+    //     this.inputText = this.text
+    //   }
+    // },
     getMySites () {
       if (!this.token) {
         return false
