6731a02d2c4316907a387cd349b1e37fafb100a6
gaoshuaixing <gaoshuaixing@bilibili.com>
2020-12-09 19:01:41 +0800

    1

--- src/views/home/single/Awesome.vue
+++ src/views/home/single/Awesome.vue
@@ -11,94 +11,49 @@
         v-model="searchContent"
         @change="resetDefault(searchContent)"
         @search="handleSearch"
-        placeholder="强大的搜索功能">
+        placeholder="">
         <a-button slot="enterButton">
           搜索
         </a-button>
       </a-input-search>
     </standard-form-row>
-    <a-card
-      style="margin-top: 0px"
-      :bordered="true"
-      title="">
-      <div class="operate">
-        <!-- <a-button type="dashed" style="width: 100%">添加后 - 在“我的”查看</a-button> -->
-        <!-- 添加后 - 在“我的”查看
-        <a-divider dashed="true" /> -->
-        <a-button type="dashed" style="width: 100%" icon="plus" @click="handleShare">分享</a-button>
-      </div>
-      <a-list :loading="loading" size="large">
-        <a-list-item :key="index" v-for="(item, index) in webList" style="padding-top: 10px;padding-bottom: 10px;">
-          <a-list-item-meta :description="item.desc">
-            <a-avatar v-if="item.img" slot="avatar" size="default" shape="square" :src="item.img"></a-avatar>
-            <a-avatar
-              v-else
-              :style="item.style || 'color: #fff;backgroundColor:#2BA245;'"
-              slot="avatar"
-              size="default"
-              shape="square"
-              :src="item.img"
-            >
-              {{ item.name.substr(0,1) }}
-            </a-avatar>
-            <a slot="title" :href="item.url" target="_blank">{{ item.name }}</a>
-          </a-list-item-meta>
-          <a-tag :key="tindex" v-for="(tag, tindex) in item.tag">{{ tag }}</a-tag>
-          <div class="list-times">
-            <div class="list-content-item">
-              <a-icon type="star-o" style="margin-right: 8px" />
-              <span>{{ item.col_times }}</span>
+    <div class="ant-pro-pages-list-applications-filterCardList">
+      <a-list :loading="loading" :data-source="projects" :grid="{ gutter: 24, xl: 4, lg: 3, md: 3, sm: 2, xs: 1 }" style="margin-top: 24px;">
+        <a-list-item slot="renderItem" slot-scope="item">
+          <a-card :body-style="{ paddingBottom: 20 }" hoverable>
+            <a-card-meta :title="item.title">
+              <!-- <template slot="avatar">
+                <a-avatar size="small" :src="item.avatar"/>
+              </template> -->
+            </a-card-meta>
+            <template slot="actions">
+              <a-tooltip title="">
+                <a-icon type="eye" /> 查看
+              </a-tooltip>
+              <a-tooltip title="">
+                <a-icon type="star" /> 256
+              </a-tooltip>
+            </template>
+            <a-tag class="tag" :key="tindex" v-for="(tag, tindex) in item.tag">{{ tag }}</a-tag>
+            <div class="description">
+              {{ item.description }}
             </div>
-          </div>
-          <!-- <div class="list-view">
-            <div class="list-content-item">
-              <a :href="item.url" target="_blank">查看</a>
-            </div>
-          </div> -->
-          <div class="list-view">
-            <a @click="handleEdit(item)">添加</a>
-          </div>
+          </a-card>
         </a-list-item>
-        <span />
-        <div slot="footer" v-if="pageInfo.next_page_url" style="text-align: center; margin-top: 1px;">
-          <a-button @click="loadMore" :loading="loadingMore">加载更多</a-button>
-        </div>
       </a-list>
-      <user-web-form
-        ref="webSaveModal"
-        :visible="visible"
-        :loading="confirmLoading"
-        :model="mdl"
-        :category="category"
-        :pMyTypes="myTypes"
-        @cancel="handleCancel"
-        @ok="handleOk"
-      />
-      <all-web-form
-        ref="allWebModal"
-        :visible="allWebVisible"
-        :loading="allWebConfirmLoading"
-        :model="allWebMdl"
-        @cancel="allWebHandleCancel"
-        @ok="allWebHandleOk"
-      />
-    </a-card>
+    </div>
   </div>
 </template>
 
 <script>
 import storage from 'store'
 import { ACCESS_TOKEN } from '@/store/mutation-types'
-import UserWebForm from './modules/UserWebForm'
-import AllWebForm from './modules/AllWebForm'
 import { outApi } from '@/api/main'
 import { StandardFormRow } from '@/components'
 
 export default {
   name: 'StandardList',
   components: {
-    UserWebForm,
-    AllWebForm,
     StandardFormRow
   },
   data () {
@@ -107,7 +62,6 @@ export default {
       category: '1',
       webList: [],
       pageInfo: {},
-      myTypes: [],
       loading: false,
       loadingMore: false,
       // 查询参数
@@ -115,19 +69,56 @@ export default {
       type2: 0,
       module: 'collection',
       desc: '',
+      projects: [
+        {
+          title: '《不可替代的团队领袖培养计划》',
+          cover: '11',
+					col_times: 32,
+					tag: ['读书', '前端'],
+          description: '《不可替代的团队领袖培养计划》leetcode/lintcode题解/算法学习sae笔记aweawe笔记afwe笔记wew笔记wewe'
+        },
+        {
+          title: 'LeetCode题解',
+          cover: '11',
+					col_times: 32,
+					tag: [],
+          description: 'LeetCode题解'
+        },
+        {
+          title: '前端开发笔记本',
+          cover: '11',
+					col_times: 32,
+					tag: ['读书', '前端'],
+          description: '前端开发笔记本'
+        },
+        {
+          title: 'leetcode/lintcode题解/算法学习笔记',
+          cover: '11',
+					col_times: 32,
+					tag: ['读书', '前端'],
+          description: 'leetcode/lintcode题解/算法学习笔记'
+        },
+        {
+          title: '借助开源项目，学习软件开发',
+          cover: '11',
+					col_times: 32,
+					tag: ['读书', '前端'],
+          description: 'miaoshu'
+        },
+        {
+          title: '老齐的技术资料',
+          cover: '11',
+					col_times: 32,
+					tag: ['读书', '前端'],
+          description: 'miaoshu'
+        }
+      ],
       visible: false,
-      confirmLoading: false,
-      allWebVisible: false,
-      allWebConfirmLoading: false,
-      mdl: null,
-      allWebMdl: null,
       searchContent: ''
     }
   },
   mounted () {
     this.getToken()
-    this.getWebFind()
-    this.getMyTypes()
   },
   methods: {
     getToken () {
@@ -144,31 +135,6 @@ export default {
       this.page = 1
       this.desc = ''
       this.getWebFind('reset')
-    },
-    handleEdit (record) {
-      if (!this.token) {
-        this.$message.error('请登录')
-        return false
-      }
-      this.visible = true
-      this.mdl = {
-        id: record.wsid,
-        name: record.name,
-        url: record.url
-      }
-    },
-    handleShare () {
-      if (!this.token) {
-        this.$message.error('请登录')
-        return false
-      }
-      this.allWebVisible = true
-      this.allWebMdl = {
-        id: 0,
-        name: '',
-        url: '',
-        desc: ''
-      }
     },
     resetDefault (value) {
       if (value === '') {
@@ -207,101 +173,6 @@ export default {
       this.loadingMore = true
       this.page += 1
       this.getWebFind()
-    },
-    getMyTypes () {
-      if (!this.token) {
-        return false
-      }
-      const params = {
-        out_url: 'myTypes',
-        method: 'POST',
-        data: {
-          category: this.category
-        }
-      }
-      outApi(params).then(res => {
-        if (res.code !== 0) {
-          return false
-        }
-        this.myTypes = res.data
-        }).catch(err => {
-          console.log('err:', err)
-        })
-    },
-    handleCancel () {
-      this.visible = false
-      this.confirmLoading = false
-      const form = this.$refs.webSaveModal.form
-    },
-    handleOk () {
-      const form = this.$refs.webSaveModal.form
-      this.confirmLoading = true
-      form.validateFields((errors, values) => {
-        // console.log('allweb values:', values)
-        if (!errors) {
-          const saveMySiteParams = {
-            out_url: 'saveMySite',
-            method: 'POST',
-            data: {
-              category: this.category,
-              wsid: values.id,
-              type: values.type,
-              name: values.name,
-              url: values.url,
-              sort: 0
-            }
-          }
-          outApi(saveMySiteParams).then(res => {
-            this.confirmLoading = false
-            if (res.code !== 0) {
-              this.$message.info('添加失败')
-              return
-            }
-              this.visible = false
-              this.$message.info('添加成功')
-            }).catch(err => {
-              console.log('err:', err)
-            })
-        } else {
-          this.confirmLoading = false
-        }
-      })
-    },
-    allWebHandleCancel () {
-      this.allWebVisible = false
-      this.allWebConfirmLoading = false
-      const form = this.$refs.allWebModal.form
-    },
-    allWebHandleOk () {
-      const form = this.$refs.allWebModal.form
-      this.allWebConfirmLoading = true
-      form.validateFields((errors, values) => {
-        console.log('allweb values:', values)
-        if (!errors) {
-          const saveAllWebParams = {
-            out_url: 'saveAllWeb',
-            method: 'POST',
-            data: {
-              name: values.name,
-              url: values.url,
-              desc: values.desc
-            }
-          }
-          outApi(saveAllWebParams).then(res => {
-            if (res.code !== 0) {
-              this.$message.info('添加失败')
-              return
-            }
-              this.$message.info('添加成功')
-              this.handleChangeType('new')
-            }).catch(err => {
-              console.log('err:', err)
-            }).finally(() => {
-              this.allWebVisible = false
-              this.allWebConfirmLoading = false
-            })
-        }
-      })
     }
   }
 }
@@ -335,5 +206,14 @@ export default {
         margin-bottom: 0;
         line-height: 22px;
     }
+}
+.tag {
+	margin-top: 10px;
+}
+.description {
+	margin-top: 10px;
+	height: 45px;
+  overflow: hidden;
+  text-align: left;
 }
 </style>
