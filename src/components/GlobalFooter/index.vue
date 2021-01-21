14d549d25854824b0cb7d27b72eac19013cc6409
gaoshuaixing <gaoshuaixing@bilibili.com>
2021-01-21 16:42:57 +0800

    1

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -1,6 +1,8 @@
 <template>
   <global-footer class="footer custom-render">
     <template v-slot:links>
+      <p>Copyright @ 2019~2021 盒子 版权所有</p>
+      <p>京ICP备15041380号-2</p>
     </template>
     <!-- <template v-slot:copyright>
       <a href="https://github.com/vueComponent" target="_blank">vueComponent</a>
